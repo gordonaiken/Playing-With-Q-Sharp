cee6d8ff7d99275e9774a01c2928f019701098a7
gordonaiken <aiken.gordon@gmail.com>
2018-08-26 18:33:39 +0100

    Basic addition operation 'add' in the qs file

---
+++ HelloQ/HelloQ/Driver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.HelloQ
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            using (var sim = new QuantumSimulator())
+            {
+                var res = Add.Run(sim, 20, 10).Result;
+                Console.WriteLine(res);
+            }
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HelloQ/HelloQ/Operation.qs
@@ -0,0 +1,13 @@
+﻿namespace Quantum.HelloQ
+{
+    open Microsoft.Quantum.Primitive;
+    open Microsoft.Quantum.Canon;
+
+    operation Add (a : Int, b : Int) : (Int)
+    {
+        body
+        {
+            return (a + b);
+        }
+    }
+}
